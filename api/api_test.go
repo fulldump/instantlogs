32148c2ede81d6d3caea113cbe290afb1e9a0676
fulldump <gerardooscarjt@gmail.com>
2022-06-12 00:13:00 +0200

    test: add api happy path test

--- api/api_test.go
+++ api/api_test.go
@@ -1,3 +1,32 @@
 package api
 
-// TODO: add some tests here...
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/fulldump/biff"
+	"github.com/fulldump/box"
+
+	"instantlogs/service"
+)
+
+// TODO: add concurrency tests here...
+
+func TestNewApi_HappyPath(t *testing.T) {
+
+	// setup
+	api := NewApi(service.NewService(), "")
+	mockserver := httptest.NewServer(box.Box2Http(api))
+	defer mockserver.Close()
+
+	// run
+	http.Post(mockserver.URL+"/ingest", "text/plain", strings.NewReader("log1\nlog2\nlog3\n"))
+
+	// check
+	resp, _ := http.Get(mockserver.URL + "/filter")
+	respBody, _ := io.ReadAll(resp.Body)
+	biff.AssertEqual(string(respBody), "log1\nlog2\nlog3\n")
+}
